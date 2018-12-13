ecc1ced5400365a700da048a7802486505ac5a4e
kenyi <nthramk@gmail.com>
2018-12-13 16:21:29 -0500

    development

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='StadisticBasic',
+    version='0.1dev',
+    packages=['stadistic_basic',],
+    license='MIT',
+    long_description=open('README.txt').read(),
+    author='Kenyi Simons',
+    url = 'https://github.com/nathramk/stadistic_basic', # use the URL to the github repo
+    download_url = 'https://github.com/nathramk/stadistic_basic/tarball/0.1',
+)
